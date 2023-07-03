0f57425b4e20d41a6f75e8c8c09a9e9f03a39b42
john32y <john.thelronge@gmail.com>
2023-07-03 11:50:19 -0400

    beers with mapping

---
+++ src/components/BeerList.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import  BeerCard from "./BeerCard"
+export default function BeerList() {
+    const [beers, setBeers] = useState()
+
+    const getBeers = () => {
+        fetch('https://api.sampleapis.com/beers/ale')
+        .then(res => res.json())
+        .then(setBeers)
+        .catch(alert)
+        
+    }
+    return (
+        <main>
+            <button onClick={getBeers}>Get Beers</button>
+            <section className='beer-list'>
+                {!beers
+                ? <h2>Pouring...</h2>
+                : beers.map((beer)=> (
+                    <BeerCard
+                    key={beer.id}
+                    name={beer.name}
+                    avgRating={beer.rating.average}
+                    image={beer.image} />
+                    
+                ))    
+                }
+            </section>
+        </main>
+    )
+}
